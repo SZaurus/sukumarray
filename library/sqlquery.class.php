00bccb0c20748eb0af96bebb79bef00946b376db
Mahmud Hasan <my.googl.drive@gmail.com>
2014-08-20 13:54:25 -0400

    added childOrder to search() function in sqlquery.class.php

--- library/sqlquery.class.php
+++ library/sqlquery.class.php
@@ -148,8 +148,9 @@
                             $fromChild .= '`'.$tableChild.'` as `'.$aliasChild.'`';
 
                             $conditionsChild .= '`'.$aliasChild.'`.`'.strtolower($this->_model).'_id` = \''.$tempResults[$this->_model]['id'].'\'';
+                            $orderBy = (isset($this->childOrderBy[$aliasChild]) ? " ORDER BY `" . $aliasChild . "`.`" . $this->childOrderBy[$aliasChild][0] . "` " . $this->childOrderBy[$aliasChild][1] . " "; 
 
-                            $queryChild =  'SELECT * FROM '.$fromChild.' WHERE '.$conditionsChild;	
+                            $queryChild =  'SELECT * FROM '.$fromChild.' WHERE '.$conditionsChild . $orderBy;
                             #echo '<!--'.$queryChild.'-->';
                             $resultChild = mysql_query($queryChild, $this->_dbHandle);
 
